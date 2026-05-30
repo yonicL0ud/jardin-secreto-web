085ea0d3d951c419d086e4ba660bcda0ffe2abf2
fernandoPold <72273879@continental.edu.pe>
2026-05-30 16:38:59 -0500

    confirmado

---
+++ js/data.js
@@ -0,0 +1,56 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Monstera Deliciosa",
+        precio: 89.00,
+        imagen: "../img/monstera.jpg", 
+        imagenHome: "img/monstera.jpg", 
+        descripcion: "Hermosa planta de hojas grandes y perforadas, ideal para salones iluminados.",
+        dificultad: "facil"
+    },
+    {
+        id: 2,
+        nombre: "Ficus Lyrata",
+        precio: 120.00,
+        imagen: "../img/ficus.jpg",
+        imagenHome: "img/ficus.jpg",
+        descripcion: "Elegante árbol de hojas con forma de lira. Aporta gran altura y diseño.",
+        dificultad: "moderado"
+    },
+    {
+        id: 3,
+        nombre: "Sansevieria Lengua",
+        precio: 45.00,
+        imagen: "../img/sansevieria.jpg",
+        imagenHome: "img/sansevieria.jpg",
+        descripcion: "Purifica el aire de forma eficiente y sobrevive a casi cualquier espacio.",
+        dificultad: "facil"
+    },
+    {
+        id: 4,
+        nombre: "Pothos de Oro",
+        precio: 35.00,
+        imagen: "../img/pothos.jpg",
+        imagenHome: "img/pothos.jpg",
+        descripcion: "Planta colgante de rápido crecimiento con sutiles destellos dorados.",
+        dificultad: "facil"
+    },
+    {
+        id: 5,
+        nombre: "Calathea Orbifolia",
+        precio: 75.00,
+        imagen: "../img/calathea.jpg",
+        imagenHome: "img/calathea.jpg",
+        descripcion: "Hojas redondas con elegantes rayas plateadas. Requiere humedad ambiental.",
+        dificultad: "moderado"
+    },
+    {
+        id: 6,
+        nombre: "Suculenta Echeveria",
+        precio: 20.00,
+        imagen: "../img/suculenta.jpg",
+        imagenHome: "img/suculenta.jpg",
+        descripcion: "Pequeña planta en forma de roseta perfecta para escritorios y ventanas.",
+        dificultad: "facil"
+    }
+];
