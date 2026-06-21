77021923493b07196657e50fb8427a4a5fc8b445
yonicL0ud <61174562@continental.edu.pe>
2026-06-20 20:05:30 -0500

    .

--- js/data.js
+++ js/data.js
@@ -3,7 +3,7 @@ const productos = [
         id: 1,
         nombre: "Monstera deliciosa",
         precio: 89,
-        imagen: "../img/monstera.jpg",
+        imagen: "/jardin-secreto-web/img/monstera.jpg",
         categoria: "interior",
         descripcion: "Planta de hojas grandes y perforadas, ideal para interiores luminosos",
         dificultad: "fácil"
@@ -12,7 +12,7 @@ const productos = [
         id: 2,
         nombre: "Pack Suculentas",
         precio: 35,
-        imagen: "../img/suculentas.jpg",
+        imagen: "/jardin-secreto-web/img/suculentas.jpg",
         categoria: "suculentas",
         descripcion: "Set de 6 suculentas mini en macetas de cerámica",
         dificultad: "fácil"
@@ -21,7 +21,7 @@ const productos = [
         id: 3,
         nombre: "Maceta Macramé",
         precio: 45,
-        imagen: "../img/macrame.jpg",
+        imagen: "/jardin-secreto-web/img/macrame.jpg",
         categoria: "decoración",
         descripcion: "Colgante artesanal de macramé para plantas",
         dificultad: "fácil"
@@ -30,7 +30,7 @@ const productos = [
         id: 4,
         nombre: "Helecho Cuero",
         precio: 65,
-        imagen: "../img/helecho.jpg",
+        imagen: "/jardin-secreto-web/img/helecho.jpg",
         categoria: "interior",
         descripcion: "Helecho que purifica el aire, necesita humedad constante",
         dificultad: "moderado"
@@ -39,7 +39,7 @@ const productos = [
         id: 5,
         nombre: "Kit Herramientas",
         precio: 29,
-        imagen: "../img/kit.jpg",
+        imagen: "/jardin-secreto-web/img/kit.jpg",
         categoria: "accesorios",
         descripcion: "Mini pala, rastrillo, tijeras y pulverizador",
         dificultad: "fácil"
@@ -48,7 +48,7 @@ const productos = [
         id: 6,
         nombre: "Sansevieria",
         precio: 55,
-        imagen: "../img/sansevieria.jpg",
+        imagen: "/jardin-secreto-web/img/sansevieria.jpg",
         categoria: "interior",
         descripcion: "Lengua de suegra, resistente y purificadora",
         dificultad: "fácil"
@@ -57,7 +57,7 @@ const productos = [
         id: 7,
         nombre: "Cactus Gigante",
         precio: 120,
-        imagen: "../img/cactus.webp",
+        imagen: "/jardin-secreto-web/img/cactus.webp",
         categoria: "suculentas",
         descripcion: "Cactus de gran tamaño, perfecto para exteriores soleados",
         dificultad: "fácil"
@@ -66,7 +66,7 @@ const productos = [
         id: 8,
         nombre: "Ficus Lyrata",
         precio: 150,
-        imagen: "../img/ficus.jpg",
+        imagen: "/jardin-secreto-web/img/ficus.jpg",
         categoria: "interior",
         descripcion: "Hojas grandes en forma de violín, muy elegante y purificadora",
         dificultad: "moderado"
